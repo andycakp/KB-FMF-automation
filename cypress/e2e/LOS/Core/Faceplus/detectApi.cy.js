573fdc981feaf3401068a562f13091dc0f388d36
andycakp <andyca.restyanto@kpvendor.id>
2023-08-22 11:02:51 +0700

    Update Code Response for timeout case

--- cypress/e2e/LOS/Core/Faceplus/detectApi.cy.js
+++ cypress/e2e/LOS/Core/Faceplus/detectApi.cy.js
@@ -123,7 +123,7 @@ describe('Faceplus Detect Integrator API ', function () {
             let result = response.body.data
             let meta = response.body.meta
             
-            expect(meta.code).to.eq(500);
+            expect(meta.code).to.eq(400);
             expect(meta.message).to.eq("Oops, Something Went Wrong");
             expect(meta.error).eq("We are Sorry, The Service Is Not Available Right Now");
             expect(result).is.null
